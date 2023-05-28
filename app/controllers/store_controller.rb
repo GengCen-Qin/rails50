18c6903d05544834362161da5a54b5be0cd519d9
秦思成-MacBook <464815411@qq.com>
2023-05-28 19:45:17 +0800

    测试： 1. 用户访问store.index 时，使用计数器统计，用户访问次数 2.用户加入购物车时，访问次数归零 3.当访问次数大于5时，显示

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,18 @@
 class StoreController < ApplicationController
+  before_action :calculate_user_counter, only: [:index]
   def index
     @products = Product.order(:title)
     @current_time = Time.now
+    @counter = session[:counter]
+  end
+
+  private
+
+  def calculate_user_counter
+    if session[:counter].nil?
+      session[:counter] = 1;
+    else
+      session[:counter] += 1;
+    end
   end
 end
