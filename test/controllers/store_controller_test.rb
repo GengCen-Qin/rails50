91619b2cd1e3a99929954667969642423e4b84b5
秦思成-MacBook <464815411@qq.com>
2023-05-28 17:10:45 +0800

    控制器功能测试

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select '.store #columns #side a', minimum: 4
+    assert_select '.store #main .entity', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 end
